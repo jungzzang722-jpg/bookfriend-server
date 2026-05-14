7191fb8a2318cdb8862b4de257e586c50f5b5d0b
jungzzang722-jpg <jungzzang722@gmail.com>
2026-05-14 21:52:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -327,7 +327,11 @@ ${emotion}
 SERVER START
 ========================================
 */
+app.use(express.static(__dirname));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 app.listen(3000,()=>{
 
   console.log("📖 책친구 서버 실행 중");
