739e2c1521c3562757c3954432eaa07f9b181bd2
jungzzang722-jpg <jungzzang722@gmail.com>
2026-05-14 22:05:08 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
 
 /*
 ========================================
